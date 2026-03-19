9e33ec9d6c0d3bb5ba6632ba461264a994b191c2
SasakurePanda <Iris.11031221@docomo.ne.jp>
2026-03-20 00:50:31 +0900

    【新規】各Playerの動き用のスクリプトを追加しました
    ・PlayerController.cs→プレイヤーの入力と基本動作
    ・UmbrellaController.cs→傘関連の動作
    ・GunController.cs→銃関連の動作
    ・GroundCheck.cs→地面についているかどうかの判定

---
+++ Assets/Scripts/Player/GroundCheck.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    [Header("地面のレイヤー")]
+    [SerializeField] private LayerMask groundLayer;
+
+    private bool isGround = false;
+    private bool isGroundEnter, isGroundStay, isGroundExit;
+
+    //接地判定を返すメソッド
+    //物理判定の更新毎に呼ぶ必要がある
+    public bool IsGround()
+    {
+        if (isGroundEnter || isGroundStay)
+        {
+            isGround = true;
+        }
+        else if (isGroundExit)
+        {
+            isGround = false;
+        }
+
+        isGroundEnter = false;
+        isGroundStay = false;
+        isGroundExit = false;
+
+        return isGround;
+    }
+
+    private bool IsInLayer(int layer)
+    {
+        return (groundLayer & (1 << layer)) != 0;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (IsInLayer(collision.gameObject.layer))
+        {
+            isGroundEnter = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (IsInLayer(collision.gameObject.layer))
+        {
+            isGroundStay = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (IsInLayer(collision.gameObject.layer))
+        {
+            isGroundExit = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine.InputSystem;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rigidBody2d;
+
+    [Header("移動設定")]
+    [SerializeField] private float groundMoveSpeed = 5.0f;
+
+    [Header("ジャンプ設定")]
+    [SerializeField] private float jumpForce = 8.0f;
+
+    [Header("地面判定")]
+    [SerializeField] private GroundCheck groundCheck;
+
+    private GunController gunController;    //銃関連のスクリプト
+    private UmbrellaController umbrellaController;  //傘関連のスクリプ
+
+    private float moveInput;
+    private bool jumpInput;
+    private bool isGround;
+
+    private void Awake()
+    {
+        rigidBody2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        gunController = GetComponentInChildren<GunController>();
+        umbrellaController = GetComponentInChildren<UmbrellaController>();
+    }
+
+    private void Update()
+    {
+        GetInput();
+        Flip();
+
+        isGround = groundCheck.IsGround();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        Jump();
+    }
+
+    private void GetInput()
+    {
+        //基本移動
+        moveInput = 0;
+        if (Keyboard.current.aKey.isPressed)
+        {
+            moveInput = -1;
+        }
+        else if (Keyboard.current.dKey.isPressed)
+        {
+            moveInput = 1;
+        }
+
+        //ジャンプ
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            jumpInput = true;
+        }
+    }
+
+    /// <summary>
+    /// プレイヤーの左右移動処理の関数
+    /// </summary>
+    private void Move()
+    {
+        Vector2 velocity = rigidBody2d.linearVelocity;
+
+        if (moveInput != 0)
+        {
+            velocity.x = moveInput * groundMoveSpeed;
+        }
+        else
+        {
+            velocity.x = 0;
+        }
+
+        rigidBody2d.linearVelocity = velocity;
+    }
+
+    /// <summary>
+    /// プレイヤーのジャンプ処理の関数
+    /// </summary>
+    private void Jump()
+    {
+        if (jumpInput)
+        {
+            if (isGround)
+            {
+                rigidBody2d.linearVelocity = new Vector2(rigidBody2d.linearVelocity.x, jumpForce);
+            }
+
+            jumpInput = false;
+        }
+    }
+
+    /// <summary>
+    /// プレイヤーの向き処理の関数
+    /// </summary>
+    private void Flip()
+    {
+         if (moveInput > 0)
+         {
+             transform.localScale = new Vector3(1, 1, 1);
+         }
+         else if (moveInput < 0)
+         {
+             transform.localScale = new Vector3(-1, 1, 1);
+         }
+
+         return;
+    }
+
+}
