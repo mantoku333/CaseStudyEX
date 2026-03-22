db3e597e963f0d43e952ea380b30829415a2d335
SasakurePanda <Iris.11031221@docomo.ne.jp>
2026-03-22 22:45:09 +0900

    【新規】パリィ用のスクリプトを追加しました
    今は追加のみでまだ入力処理を入れていないので動いていないです。

---
+++ Assets/Scripts/Player/ParryHitbox.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using Cysharp.Threading.Tasks;
+
+public class ParryHitbox : MonoBehaviour
+{
+    [Header("ヒットストップ時間")]
+    [SerializeField] private float hitStopTime = 0.05f;
+
+    private async void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("EnemyBullet"))
+        {
+            Debug.Log("パリィ成功！");
+
+            Rigidbody2D rigidBody2D = collision.GetComponent<Rigidbody2D>();
+
+            if (rigidBody2D != null)
+            {
+                //プレイヤーから見た方向に反射
+                Vector2 direction = (collision.transform.position - transform.position).normalized;
+                float speed = rigidBody2D.linearVelocity.magnitude;
+
+                rigidBody2D.linearVelocity = direction * speed;
+            }
+
+            //ヒットストップ
+            Time.timeScale = 0f;
+
+            await UniTask.Delay((int)(hitStopTime * 1000), ignoreTimeScale: true);
+
+            Time.timeScale = 1f;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/UmbrellaParryController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using Cysharp.Threading.Tasks;
+
+public class UmbrellaParryController : MonoBehaviour
+{
+    [Header("パリィ時間")]
+    [SerializeField] private float parryDuration = 0.1f;
+
+    [Header("当たり判定")]
+    [SerializeField] private Collider2D parryCollider;
+
+    private bool isParrying = false;
+
+    private void Awake()
+    {
+        if (parryCollider != null)
+        {
+            parryCollider.enabled = false;
+        }
+    }
+
+    /// <summary>
+    /// パリィ時の処理を行う関数
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public async UniTaskVoid Parry()
+    {
+        if (isParrying)
+        {
+            return;
+        }
+
+        Debug.Log("パリィ開始");
+
+        isParrying = true;
+
+        if (parryCollider != null)
+        {
+            parryCollider.enabled = true;
+        }
+
+        await UniTask.Delay((int)(parryDuration * 1000));
+
+        if (parryCollider != null)
+        {
+            parryCollider.enabled = false;
+        }
+
+        isParrying = false;
+
+        Debug.Log("パリィ終了");
+    }
+
+    /// <summary>
+    /// パリィ状態かどうかを返す関数
+    /// </summary>
+    /// <returns>今パリィしているかどうかのbool型</returns>
+    public bool IsParrying()
+    {
+        return isParrying;
+    }
+}
