34be732924a94164514edf91170b8590161325c7
yukiozono <yukionfire@gmail.com>
2026-05-20 17:19:52 +0900

    Revert "Merge origin/Develop and resolve conflict files"
    This reverts commit c94e31f39a7d6c5f89fc6dc3bcd45ea88a483c57.

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -14,6 +14,7 @@ namespace Player
 
         private int currentHealth;
         private float nextDamageTime;
+        private bool deathNotified;
 
         /// <summary>
         /// HPが変化したときに通知
@@ -21,6 +22,11 @@ namespace Player
         /// 第2引数: 最大HP
         /// </summary>
         public event Action<int, int> HealthChanged;
+
+        /// <summary>
+        /// HPが0になったときに1回だけ通知
+        /// </summary>
+        public event Action Died;
 
         /// <summary>現在HP</summary>
         public int CurrentHealth => currentHealth;
@@ -30,6 +36,7 @@ namespace Player
         {
             get
             {
+                TryResolveStatsData();
                 int baseMaxHealth = 1;
 
                 if (statsData != null)
@@ -39,6 +46,11 @@ namespace Player
 
                 return baseMaxHealth + maxHealthBonus;
             }
+        }
+
+        private void Awake()
+        {
+            TryResolveStatsData();
         }
 
         /// <summary>
@@ -144,6 +156,33 @@ namespace Player
         private void NotifyHealthChanged()
         {
             HealthChanged?.Invoke(currentHealth, MaxHealth);
+
+            if (currentHealth <= 0)
+            {
+                if (!deathNotified)
+                {
+                    deathNotified = true;
+                    Died?.Invoke();
+                }
+            }
+            else
+            {
+                deathNotified = false;
+            }
+        }
+
+        private void TryResolveStatsData()
+        {
+            if (statsData != null)
+            {
+                return;
+            }
+
+            var playerController = GetComponent<global::PlayerController>();
+            if (playerController != null)
+            {
+                statsData = playerController.GetPlayerStatsData();
+            }
         }
     }
 }
