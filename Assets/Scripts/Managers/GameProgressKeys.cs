fb719b65dfc10322fbdb2dad64ba245a32fdb531
yukiozono <yukionfire@gmail.com>
2026-05-29 22:49:54 +0900

    ラスボスが湧かない問題を修正

--- Assets/Scripts/Managers/GameProgressKeys.cs
+++ Assets/Scripts/Managers/GameProgressKeys.cs
@@ -1,6 +1,8 @@
 ﻿public static class GameProgressKeys
 {
     public const string Boss01Defeated = "boss_01_defeated";
+    public const string Boss02Defeated = "boss_02_defeated";
+    public const string LastBossDefeated = "last_boss_defeated";
     public const string OpenedBlueDoor = "opened_blue_door";
     public const string TutorialCompleted = "tutorial_completed";
     public const string TutorialAttackShown = "tutorial_attack_shown";
