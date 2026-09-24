e003d5b7574d7e46a769a01786418d80484676e6
mantoku333 <mantoku714@gmail.com>
2026-09-24 22:40:18 +0900

    Adjust Nox portrait placement and support paired Iris layout

--- Assets/Editor/DialogueArtworkTests.cs
+++ Assets/Editor/DialogueArtworkTests.cs
@@ -12,6 +12,64 @@ using UnityEngine.UI;
 public sealed class DialogueArtworkTests
 {
     [Test]
+    public void Nox_MovesBetweenSidesWithoutReplacingIrisAndResetsNextConversation()
+    {
+        var root = PrefabUtility.LoadPrefabContents("Assets/Prefabs/UI/StoryEventCanvas.prefab");
+        try
+        {
+            var view = root.GetComponentInChildren<DialogueView>(true);
+            var serialized = new SerializedObject(view);
+            const BindingFlags flags = BindingFlags.NonPublic | BindingFlags.Instance;
+            var apply = typeof(DialogueView).GetMethod("ApplySpeaker", flags);
+            var metadata = typeof(DialogueView).GetMethod("ApplyNoxPlacementMetadata", flags);
+            var iris = (Image)serialized.FindProperty("rightPortraitImage").objectReferenceValue;
+            var left = (Image)serialized.FindProperty("leftPortraitImage").objectReferenceValue;
+            var name = (Image)serialized.FindProperty("speakerNameArtwork").objectReferenceValue;
+            view.OnDialogueStartedAsync();
+            apply.Invoke(view, new object[] { "イリス", "default" });
+            var irisSprite = iris.sprite;
+            var originalPosition = iris.rectTransform.anchoredPosition;
+            var originalRotation = iris.rectTransform.localRotation;
+            apply.Invoke(view, new object[] { "ノクス", "default" });
+            var nox = (Image)typeof(DialogueView).GetField("_noxPortraitImage", flags).GetValue(view);
+            Assert.That(nox.rectTransform.anchorMin.x, Is.EqualTo(0));
+            Assert.That(left.gameObject.activeSelf, Is.False, "Do not leave a duplicate umbrella in the shared left slot.");
+            Assert.That(nox.rectTransform.anchoredPosition, Is.EqualTo(new Vector2(250f, 250f)));
+            Assert.That(nox.rectTransform.sizeDelta, Is.EqualTo(new Vector2(418f, 525f)));
+            Assert.That(Mathf.DeltaAngle(nox.rectTransform.localEulerAngles.z, 20f), Is.EqualTo(0f).Within(.01f));
+            apply.Invoke(view, new object[] { "タナトス", "default" });
+            Assert.That(nox.rectTransform.anchorMin.x, Is.EqualTo(1));
+            Assert.That(Mathf.DeltaAngle(nox.rectTransform.localEulerAngles.z, -8.92f), Is.EqualTo(0f).Within(.01f));
+            apply.Invoke(view, new object[] { "ノクス", "default" });
+            Assert.That(iris.gameObject.activeSelf, Is.True);
+            Assert.That(iris.sprite, Is.SameAs(irisSprite));
+            Assert.That(nox.transform.GetSiblingIndex(), Is.LessThan(iris.transform.GetSiblingIndex()));
+            Assert.That(name.sprite, Is.EqualTo(serialized.FindProperty("noxRightNameSprite").objectReferenceValue));
+            Assert.That(nox.rectTransform.anchoredPosition, Is.EqualTo(new Vector2(-113.4f, 433.9f)));
+            Assert.That(iris.rectTransform.anchoredPosition, Is.EqualTo(new Vector2(-180.5f, 280f)));
+            Assert.That(Mathf.DeltaAngle(iris.rectTransform.localEulerAngles.y, 180f), Is.EqualTo(0f).Within(.01f));
+            var animation = (PortraitAnimationClip)serialized.FindProperty("characterPortraits").GetArrayElementAtIndex(1).FindPropertyRelative("portraitAnimation").objectReferenceValue;
+            typeof(DialogueView).GetMethod("AdvancePortraitAnimations", flags).Invoke(view, new object[] { .15f });
+            Assert.That(nox.sprite, Is.EqualTo(animation.GetFrame(.15d)));
+            metadata.Invoke(view, new object[] { new[] { "nox:left" } });
+            apply.Invoke(view, new object[] { "ノクス", "default" });
+            Assert.That(nox.rectTransform.anchorMin.x, Is.EqualTo(0));
+            Assert.That(name.sprite, Is.EqualTo(serialized.FindProperty("noxLeftNameSprite").objectReferenceValue));
+            Assert.That(iris.rectTransform.anchoredPosition, Is.EqualTo(originalPosition));
+            Assert.That(Quaternion.Angle(iris.rectTransform.localRotation, originalRotation), Is.LessThan(.01f));
+            metadata.Invoke(view, new object[] { new[] { "nox:right" } });
+            view.OnDialogueCompleteAsync();
+            Assert.That(nox.gameObject.activeSelf, Is.False);
+            Assert.That(iris.rectTransform.anchoredPosition, Is.EqualTo(originalPosition));
+            Assert.That(Quaternion.Angle(iris.rectTransform.localRotation, originalRotation), Is.LessThan(.01f));
+            view.OnDialogueStartedAsync();
+            apply.Invoke(view, new object[] { "ノクス", "default" });
+            Assert.That(nox.rectTransform.anchorMin.x, Is.EqualTo(0));
+        }
+        finally { PrefabUtility.UnloadPrefabContents(root); }
+    }
+
+    [Test]
     public void DialogueText_UsesThirtyByTwoOrTwentyByOneWithoutShrinking()
     {
         var root = PrefabUtility.LoadPrefabContents("Assets/Prefabs/UI/StoryEventCanvas.prefab");
